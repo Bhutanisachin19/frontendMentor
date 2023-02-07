f5b2e4232f1991a4614a5a462a135488e063bf12
Bhutanisachin19 <sachin.bhutani60@gmail.com>
2023-02-07 22:45:04 +0530

    nft preview card in progress

---
+++ Nft Preview Card/Code/nft-preview-card/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CustomCard from "./CustomCard/CustomCard";
+
+function App() {
+  return (
+    <div className="App">
+      <CustomCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ Nft Preview Card/Code/nft-preview-card/src/CustomCard/CustomCard.js
@@ -0,0 +1,43 @@
+import "./CustomCard.css";
+import CardImage from "../images/image-equilibrium.jpg";
+import etherIcon from "../images/icon-ethereum.svg";
+import clockIcon from "../images/icon-clock.svg";
+import imageAvatar from "../images/image-avatar.png";
+
+const CustomCard = () => {
+  return (
+    <div className="container">
+      <img className="image-style" src={CardImage} />
+
+      <p className="name-p">Equilibrium #3429</p>
+
+      <p className="line-p">
+        Our Equilibrium collection promotes balance and calm.
+      </p>
+
+      <div className="info-div">
+        <span className="img-txt-wrapper">
+          <img className="icon-img-tag" src={etherIcon} />
+          <span className="span-1">0.041ETH</span>
+        </span>
+
+        <span className="img-txt-wrapper">
+          <img className="icon-img-tag" src={clockIcon} />
+          <span className="span-2">3 days left</span>
+        </span>
+      </div>
+
+      <hr className="hr-tag"></hr>
+
+      <div className="lastContentWrapper">
+        <img className="avatarImage" src={imageAvatar} />
+        <span className="span-wrapper">
+          <span className="span-wrapper-child-1">Creation of</span>
+          <span className="span-wrapper-child-2">Jules Wyvern</span>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CustomCard;
